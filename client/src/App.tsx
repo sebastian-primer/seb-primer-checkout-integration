96ab53bbfa29f6cf889a614bf10483d554260fef
Seb Preston <107333242+sebastian-primer@users.noreply.github.com>
2022-06-17 15:24:56 +0900

    Render checkout and implement loading state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,19 @@
+import useClientSession from "./hooks/useClientSession";
+import Checkout from "./Checkout";
+
 function App() {
+  const { clientSession } = useClientSession();
+
+  if (!clientSession || !clientSession.clientToken) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div id="container">
-      Hello Primer
+    <div>
+      <h1>Here is the checkout:</h1>
+      <Checkout clientToken={clientSession.clientToken} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
